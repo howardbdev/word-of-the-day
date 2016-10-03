d3249838d81b7ee7c2c6bdd1128b64a793019270
Howard DeVennish <howarddevennish@gmail.com>
2016-10-03 17:41:00 +0000

    add dependencies, create cli and words files

--- lib/word_of_the_day.rb
+++ lib/word_of_the_day.rb
@@ -1,4 +1,12 @@
 require "word_of_the_day/version"
+require 'nokogiri'
+require 'pry'
+require 'open-uri'
+
+require_relative "./word_of_the_day/version"
+require_relative "./word_of_the_day/cli"
+require_relative ".word_of_the_day/words"
+
 
 module WordOfTheDay
   # Your code goes here...
